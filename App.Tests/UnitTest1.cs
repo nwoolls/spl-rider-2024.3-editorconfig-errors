dc5e529fb1b57645601294abec051142e000cb2f
Nate Woolls <nathanial.woolls@idmworks.com>
2024-11-13 13:01:12 -0500

    Add code analysis and style enforcement
    Enabled recommended code analysis settings including treating warnings as errors and enforcing code style in both main and test projects. Updated .editorconfig for file-scoped namespace suggestions and addressed minor formatting issues.

--- App.Tests/UnitTest1.cs
+++ App.Tests/UnitTest1.cs
@@ -1,10 +1,18 @@
 namespace App.Tests;
 
+// With Rider 2024.3, no errors show in the editor below, but this file has an error in the Solution Explorer.
+// Checking the Problems tool window, under All Solution Files, there's an error for this file:
+//   <App.Tests>\UnitTest1.cs:378 Anonymous function can be made static
+// However, clicking the File tab on the Problems tool window shows no errors.
+// In a large solution, this problem manifests *everywhere*.
+
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
+        List<string> list = ["foo"];
 
+        Assert.Contains(list, l => l == "foo");
     }
 }
--- App/Program.cs
+++ App/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var guid = Guid.NewGuid(); // Shows as an error in the editor and Solution Explorer - not the case in Rider 2024.2
+Console.WriteLine(guid);
