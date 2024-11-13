87534be5499152153bea7f7c48e9c4031d13627a
Nate Woolls <nathanial.woolls@idmworks.com>
2024-11-13 13:04:32 -0500

    Fix false editor errors for Guid usage and static function
    Updated comments to clarify that certain false error warnings appear in some IDE contexts but not others. Details provided for discrepancies across Rider 2024.2 and VS Code environments.

--- App.Tests/UnitTest1.cs
+++ App.Tests/UnitTest1.cs
@@ -5,6 +5,7 @@ namespace App.Tests;
 //   <App.Tests>\UnitTest1.cs:378 Anonymous function can be made static
 // However, clicking the File tab on the Problems tool window shows no errors.
 // In a large solution, this problem manifests *everywhere*.
+// This does not happen in Rider 2024.2. This also does not show in VS Code w/ C# Dev Kit.
 
 public class UnitTest1
 {
--- App/Program.cs
+++ App/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-var guid = Guid.NewGuid(); // Shows as an error in the editor and Solution Explorer - not the case in Rider 2024.2
+// Shows as an error in the editor and Solution Explorer - not the case in Rider 2024.2.
+// This does show as an error in VS Code w/ C# Dev Kit, but not in the Solution Explorer in VS Code. Only the editor.
+var guid = Guid.NewGuid();
 Console.WriteLine(guid);
